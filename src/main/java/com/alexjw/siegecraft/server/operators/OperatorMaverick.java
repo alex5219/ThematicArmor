2c3218fc3b88c2dc64e421aae3b37ddf9411030a
Alex <alex.j.witkowskI@gmail.com>
2020-06-19 19:21:21 -0400

    - Unhid Maverick
    Signed-off-by: Alex <alex.j.witkowskI@gmail.com>

--- src/main/java/com/alexjw/siegecraft/server/operators/OperatorMaverick.java
+++ src/main/java/com/alexjw/siegecraft/server/operators/OperatorMaverick.java
@@ -9,10 +9,7 @@ public class OperatorMaverick extends Operator {
     public OperatorMaverick() {
         super(Team.ATTACKER, 3, 1, 2);
     }
-
-    @Override
-    public boolean isHidden() {return true;}
-
+    
     public ArrayList<Gadget> getGadgets() {
         ArrayList<Gadget> gadgets = new ArrayList<>();
         gadgets.add(new GadgetBlowTorch());
