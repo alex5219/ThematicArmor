7c8c66e55ab66d69c59d280d05509f5bd1ffb47e
Alex <alex.j.witkowskI@gmail.com>
2020-11-11 13:46:01 -0500

    Specialists Multiple

--- src/main/java/com/alexjw/thematicarmor/server/data/TAData.java
+++ src/main/java/com/alexjw/thematicarmor/server/data/TAData.java
@@ -5,11 +5,11 @@ import net.minecraft.item.ItemStack;
 
 import java.util.HashMap;
 
-public class TAData<K, V> extends HashMap<K, V> {
-    public static TAData<EntityPlayer, Integer> TIME_DEAD = new TAData<>();
-    public static TAData<EntityPlayer, Boolean> HAS_DIED = new TAData<>();
-    public static TAData<EntityPlayer, Integer> LAST_HEAL = new TAData<>();
-    public static TAData<EntityPlayer, Boolean> TENACITY = new TAData<>();
+public class TAData<V> extends HashMap<EntityPlayer, V> {
+    public static TAData<Integer> TIME_DEAD = new TAData<>();
+    public static TAData<Boolean> HAS_DIED = new TAData<>();
+    public static TAData<Integer> LAST_HEAL = new TAData<>();
+    public static TAData<Boolean> TENACITY = new TAData<>();
 
     public int getInteger(EntityPlayer entityPlayer) {
         return (int) get(entityPlayer);
