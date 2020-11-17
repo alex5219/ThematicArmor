6eeb7fe78fb195b1466fba388716b2385ff9b014
bbovard <brendenbovard2@gmail.com>
2020-11-17 02:51:09 -0500

    fixed arm Z-fighting on some suits

--- src/main/java/com/alexjw/thematicarmor/client/model/games/ModelMario.java
+++ src/main/java/com/alexjw/thematicarmor/client/model/games/ModelMario.java
@@ -17,11 +17,7 @@ public class ModelMario extends ModelBiped {
         super(0.1f, 0.0f, 128, 128);
         textureWidth = 128;
         textureHeight = 128;
-        this.bipedLeftArm.mirror = false;
-        this.bipedLeftArm = new ModelRenderer(this);
-        this.bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
-        this.bipedLeftArm.cubeList.add(new ModelBox(bipedLeftArm, 32, 48, -1.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F, false));
-        this.bipedLeftArm.mirror = false;
+
 
         bipedHeadDetail = new ModelRenderer(this);
         bipedHeadDetail.setRotationPoint(0.0F, 24.0F, 0.0F);
